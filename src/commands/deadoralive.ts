45db1423fec3fa3932343498a8080b04c2ef113c
AntiCopium <husambunni2016@gmail.com>
2022-10-16 10:39:50 -0400

    Update deadoralive.ts

--- src/commands/deadoralive.ts
+++ src/commands/deadoralive.ts
@@ -3,15 +3,24 @@ import { format } from "https://deno.land/std@0.91.0/datetime/mod.ts";
 import Embeds from "https://deno.land/x/discordeno@17.0.0/packages/embeds/mod.ts";
 import * as mod from "https://deno.land/x/random@v1.1.2/Random.js";
 import {
+  ApplicationCommandOptionTypes,
   ApplicationCommandTypes,
-  InteractionResponseTypes,
+  InteractionResponseTypes
 } from "../../deps.ts";
 import { createCommand } from "./mod.ts";
-
 createCommand({
   name: "doa",
   description: "rtsrs has 1 in 8 chance of dead",
   type: ApplicationCommandTypes.ChatInput,
+  options: [
+    {
+      type: ApplicationCommandOptionTypes.User,
+      name: "user",
+      description:"a user to test",
+      required: false,
+
+    },
+  ],
   execute: async (Bot, interaction) => {
     const day = format(new Date(), "HH:mm");
     const embedDead = new Embeds()
@@ -44,6 +53,7 @@ createCommand({
         type: InteractionResponseTypes.ChannelMessageWithSource,
         data: {
           embeds: r,
+          
         },
       },
     );
