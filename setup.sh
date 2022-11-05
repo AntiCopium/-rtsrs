10c476338541cf293854fffbd35d093381d84156
cantevenread <husambunni2016@gmail.com>
2022-11-05 17:56:50 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -6,3 +6,4 @@ mkdir db/WarnCase
 mkdir db/WarnViolations
 deno task run
 echo "Run /resetcase command in Discord; After RESTART THE BOT"
+echo "After; run this command in cmd deno task run to start the bot. (ONLY RUN SETUP ON INITAL RUN)"
