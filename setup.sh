d2fa5919b182d52a329c24afa977c08b533e4970
cantevenread <husambunni2016@gmail.com>
2022-11-05 17:51:20 -0400

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,8 @@
+echo "Setting up project..."
+mkdir db
+mkdir db/MuteCase
+mkdir db/Violations
+mkdir db/WarnCase
+mkdir db/WarnViolations
+deno task run
+echo "Run /resetcase command in Discord; After RESTART THE BOT"
