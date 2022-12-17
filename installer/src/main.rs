f353b2fa86c6ced5a65b45a5a7312eef5834fcfa
cantevenread <husambunni2016@gmail.com>
2022-12-17 09:44:35 -0500

    better installer

--- installer/src/main.rs
+++ installer/src/main.rs
@@ -1,29 +1,63 @@
+use git2::Repository;
 use std::env;
 use std::fs;
 use std::path::PathBuf;
 use std::process;
 use std::process::Command;
+use std::process::Stdio;
+
+fn check_deno_installed() -> bool {
+    let output = Command::new("where")
+        .arg("deno")
+        .output()
+        .expect("failed to execute process");
+
+    return output.status.success();
+    println!("{:?}", output);
+}
+
+fn install_deno() {
+    let output = Command::new("curl")
+        .arg("-fsSL")
+        .arg("https://deno.land/x/install/install.sh")
+        .stdout(Stdio::piped())
+        .spawn()
+        .expect("failed to execute process");
+
+    let output = output.wait_with_output().expect("failed to wait on child");
+
+    let output = String::from_utf8(output.stdout).unwrap();
+
+    let output = Command::new("sh")
+        .arg("-c")
+        .arg(output)
+        .output()
+        .expect("failed to execute process");
+
+    assert!(output.status.success());
+}
 
 fn main() {
-
+    println!("Checking Deno Installation");
+    if !check_deno_installed() {
+        println!("Installing Deno");
+        install_deno();
+    } else {
+        println!("Deno Already Installed");
+    }
     let repo_url = "https://github.com/cantevenread/-rtsrs.git";
     let repo_name = "-rtsrs";
 
     let current_dir = env::current_dir().unwrap();
     let repo_dir = current_dir.join(repo_name);
-
+    println!("Cloning Repo...");
     if !repo_dir.exists() {
-        let output = Command::new("git")
-            .arg("clone")
-            .arg(repo_url)
-            .output()
-            .expect("failed to execute process");
-
-        println!("{}", String::from_utf8_lossy(&output.stdout));
-
-        let status = output.status;
-        println!("Status: {}", status);
+        let repo = match Repository::clone(repo_url, current_dir.join("-rtsrs")) {
+            Ok(repo) => repo,
+            Err(e) => panic!("fail clone {}", e),
+        };
     }
+    println!("Repo cloned");
 
     println!("Enter your bot token: ");
     let mut token_input = String::new();
