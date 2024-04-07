296a3abe16be5b46c9fd45676837c875a8fa0fd0
CanRead <78627491+cantevenread@users.noreply.github.com>
2024-04-07 18:56:20 -0400

    t

--- src/Rtsrs.UserConfig/mod.ts
+++ src/Rtsrs.UserConfig/mod.ts
@@ -1,12 +1,27 @@
+import { writeJson } from "https://deno.land/std@0.66.0/fs/write_json.ts";
+
 export enum UserConfigOptions {
     MessageDeletionLogSetting = "MessageDeletionLogSetting"
 }
 
+// config resets on bot reset. Will fix later
 
-export let UserConfigSettings = new Map([
+export const UserConfigSettings = new Map([
     /*
     DEFAULTS: 
         MessageDeletionLogSetting: false
     */
     [UserConfigOptions.MessageDeletionLogSetting, false]
 ])
+
+setInterval(async function() {
+ const data = Object.fromEntries(UserConfigSettings);
+  console.log(data);
+  
+  try {
+    await writeJson('./src/Rtsrs.UserConfig/UserConfigSettings.json', data, { create: true, spaces: 2}); // Write the JSON data to the file
+    console.log('UserConfigSettings.json updated successfully!');
+  } catch (error) {
+    console.error('Error writing to UserConfigSettings.json:', error);
+  }
+}, 2 * 1000); 
